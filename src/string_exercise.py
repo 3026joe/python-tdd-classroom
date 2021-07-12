d45a051be0a0d8bec2109aa6b963e7bcae2849cf
3026joe <jd.cherukara@gmail.com>
2021-07-12 16:00:28 +0530

    implemented functions

--- src/string_exercise.py
+++ src/string_exercise.py
@@ -7,25 +7,35 @@ class StringExercise:
         """
         Reverses order of characters in string input_str.
         """
-        return None
+        input_str = input_str[::-1]
+        return input_str
 
     def is_english_vowel(self, character):
         """
         Returns True if character is an english vowel
         and False otherwise.
         """
-        return None
-
-    def find_longest_word(self, sentence):
-        """
-        Returns the longest word in string sentence.
-        In case there are several, return the first.
-        """
-        return None
+        l = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+        if( character in l):
+            return True
+        return False
 
     def get_word_lengths(self, text):
         """
         Returns a list of integers representing
         the word lengths in string text.
         """
-        return None
+        l = text.split(" ")
+        lens = list(map(lambda i: len(i), l))
+        return lens
+
+    def find_longest_word(self, sentence):
+        """
+        Returns the longest word in string sentence.
+        In case there are several, return the first.
+        """
+        l = sentence.split(" ")
+        lens = self.get_word_lengths(sentence)
+        max_len = max(lens)
+        max_index = lens.index(max_len)
+        return l[max_index]
